49ad16e15d18e07b3a8d75e9a167fdb063ddcbf4
VasilisPngs <110207897+VasilisPngs@users.noreply.github.com>
2026-09-11 09:05:47 +0300

    feat: add APKMirror release page service

---
+++ app/src/main/kotlin/com/android/apkupdater/data/api/ApkMirrorPageService.kt
@@ -0,0 +1,25 @@
+package com.android.apkupdater.data.api
+
+import okhttp3.ResponseBody
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Url
+
+interface ApkMirrorPageService {
+
+    @Headers("User-Agent: APKUpdater/1.0.0")
+    @GET
+    suspend fun getReleasePage(@Url url: String): ResponseBody
+
+    companion object {
+        private const val BASE_URL = "https://www.apkmirror.com/"
+
+        fun create(): ApkMirrorPageService = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+            .create(ApkMirrorPageService::class.java)
+    }
+}
