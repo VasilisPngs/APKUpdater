a2baf05f014ee79df92a4634be748436719e98fe
VasilisPngs <110207897+VasilisPngs@users.noreply.github.com>
2026-09-12 12:35:41 +0300

    chore: migrate MainActivity to GUpdater package

--- app/src/main/kotlin/com/android/apkupdater/MainActivity.kt
+++ app/src/main/kotlin/com/android/apkupdater/MainActivity.kt
@@ -1,13 +1,13 @@
-package com.android.apkupdater
+package com.android.gupdater
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import com.android.apkupdater.ui.ApkUpdaterScreen
-import com.android.apkupdater.ui.ApkUpdaterViewModel
-import com.android.apkupdater.ui.theme.GUpdaterTheme
+import com.android.gupdater.ui.ApkUpdaterScreen
+import com.android.gupdater.ui.ApkUpdaterViewModel
+import com.android.gupdater.ui.theme.GUpdaterTheme
 
 class MainActivity : ComponentActivity() {
     private val viewModel: ApkUpdaterViewModel by viewModels()
