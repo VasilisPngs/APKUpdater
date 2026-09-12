065bc1167fa7d202b38b96ad22339a117f9e4971
VasilisPngs <110207897+VasilisPngs@users.noreply.github.com>
2026-09-12 13:28:38 +0300

    Place Manual button left and Update button right

--- app/src/main/kotlin/com/android/gupdater/ui/GUpdaterScreen.kt
+++ app/src/main/kotlin/com/android/gupdater/ui/GUpdaterScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -314,20 +314,20 @@ private fun AppListItem(
             }
             Spacer(Modifier.height(16.dp))
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-                FilledTonalButton(
-                    onClick = { openUrlInBrowser(context, update.apkMirrorUrl) },
-                    enabled = !installActive,
-                    shape = MaterialTheme.shapes.extraLarge
-                ) {
-                    Text(stringResource(R.string.update))
-                }
-                Spacer(Modifier.width(8.dp))
                 OutlinedButton(
                     onClick = onManualUpdate,
                     enabled = !installActive,
                     shape = MaterialTheme.shapes.extraLarge
                 ) {
                     Text(stringResource(R.string.manual))
+                }
+                Spacer(Modifier.width(8.dp))
+                FilledTonalButton(
+                    onClick = { openUrlInBrowser(context, update.apkMirrorUrl) },
+                    enabled = !installActive,
+                    shape = MaterialTheme.shapes.extraLarge
+                ) {
+                    Text(stringResource(R.string.update))
                 }
             }
         }
